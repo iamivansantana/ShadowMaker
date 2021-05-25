8b6e5a7eef13fc6e6ccb587779787496c588951b
unknown <iam.ivansantana@gmail.com>
2021-05-25 14:58:57 -0500

    shadowMaker added coments (25/05/2021)

--- src/components/ShadowScreen.js
+++ src/components/ShadowScreen.js
@@ -4,27 +4,23 @@ import RangeBar from './rangeBar/RangeBar';
 
 const ShadowScreen = () => {
 
+    //se utiliza un useEffect para crear los Id de los elementos html despues de la creacion del documento html
+    //Para más información checar el ciclo de vida de un componente en react
    useEffect(() => {
-        //Elementos HTML
+        //Creacion de referencia a Elementos HTML.
         const caja = document.getElementById('caja');
         const sliderOpacity = document.getElementById('myRangeOpacity');                
         const sliderWidth = document.getElementById('myRangeWidth');        
         const sliderHeight = document.getElementById('myRangeHeight');        
         const sliderRadius = document.getElementById('myRangeRadius');       
-        const sliderPropagation = document.getElementById('myRangePropagation');        
-        const valueRed1 = document.getElementById('valueRed1');
-        const valueRed2 = document.getElementById('valueRed2');
-        const valueRed3 = document.getElementById('valueRed3');
-        const valueGreen1 = document.getElementById('valueGreen1');
-        const valueGreen2 = document.getElementById('valueGreen2');
-        const valueGreen3 = document.getElementById('valueGreen3');
-        const valueBlue1 = document.getElementById('valueBlue1');
-        const valueBlue2 = document.getElementById('valueBlue2');
-        const valueBlue3 = document.getElementById('valueBlue3');
-
+        const sliderPropagation = document.getElementById('myRangePropagation');  
         
-
-       //InputColor-Background Color
+        const valueRedClass = document.querySelectorAll('.valueRedClass');     
+        const valueGreenClass = document.querySelectorAll('.valueGreenClass');     
+        const valueBlueClass = document.querySelectorAll('.valueBlueClass');     
+        
+       //InputColor-Background Color.
+            //Elemento Input tipo color para definir el color del fondo.
         const backColorInputColor= document.getElementById('backColorInputColor');
         backColorInputColor.addEventListener('input', e => {
             let background = document.getElementById('fondo');
@@ -32,6 +28,7 @@ const ShadowScreen = () => {
             document.getElementById('backColorInputText').value = e.target.value;
         });
         //InputText-Background Color
+            //Elemento Input tipo text para definir el color del fondo.
         const backColorInputText= document.getElementById('backColorInputText');
         backColorInputText.addEventListener('input', e => {
             let background = document.getElementById('fondo');
@@ -40,12 +37,14 @@ const ShadowScreen = () => {
         });
 
         //InputColor-Box Color
+            //Elemento Input tipo color para definir el color del la caja.
        const boxColorInputColor= document.getElementById('boxColorInputColor');
        boxColorInputColor.addEventListener('input', e => {
             caja.style.background = e.target.value;
             document.getElementById('boxColorInputText').value = e.target.value;
         });
         //InputText-Box Color
+            //Elemento Input tipo text para definir el color del la caja.
         const boxColorInputText= document.getElementById('boxColorInputText');
         boxColorInputText.addEventListener('input', e => {
             caja.style.background = e.target.value;
@@ -53,6 +52,7 @@ const ShadowScreen = () => {
         });
 
         //InputColor-Shadow Color
+            //Elemento Input tipo color para definir el color del la sombra.
        const shadowcolorInputColor= document.getElementById('shadowcolorInputColor');
        shadowcolorInputColor.addEventListener('input', e => {
 
@@ -67,40 +67,50 @@ const ShadowScreen = () => {
            //Valor del SliderPropagation (Barra).
            const valueSliderPropagation = sliderPropagation.value;
 
-            const inputValue = e.target.value;
-
-            const color = inputValue;
+           //Asignacion del valor target a const color donde color es una string como -> ("#a5a8b1")
+           const color = e.target.value;
+            
+           //El valor 1 y 2 del string corresponden al valor exadecimal del color red
             const red = color[1]+color[2];
+            //El valor 3 y 4 del string corresponden al valor exadecimal del color green
             const green = color[3]+color[4];
+             //El valor 5 y 6 del string corresponden al valor exadecimal del color blue
             const  blue= color[5]+color[6];
 
+            //Los string se convierten en numeros base 16
             const r = parseInt(red,16);
             const g = parseInt(green,16);
             const b = parseInt(blue,16);
 
-            valueRed1.innerHTML = r;
-            valueRed2.innerHTML = r;
-            valueRed3.innerHTML = r;
+            //para cada elemto con la clase valueRedClass se le asigna el valor de r
+            valueRedClass.forEach(function(elemento, indice, array) {                                        
+                    elemento.innerHTML = r;                                     
+                });
+            //para cada elemeto con la clase valuegreenClass se le asigna el valor de g
+            valueGreenClass.forEach(function(elemento, indice, array) {                                        
+                    elemento.innerHTML = g;                                     
+                });
+            //para cada elemeto con la clase valueBlueClass se le asigna el valor de b
+            valueBlueClass.forEach(function(elemento, indice, array) {                                        
+                    elemento.innerHTML = b;                                     
+                });
 
-            valueGreen1.innerHTML = g;
-            valueGreen2.innerHTML = g;
-            valueGreen3.innerHTML = g;
-
-            valueBlue1.innerHTML = b;
-            valueBlue2.innerHTML = b;
-            valueBlue3.innerHTML = b;
-
-
+            //Se asigna a value la estructura con los valores que se van modifiando para la sombra de la caja.
            const value = ` ${valueSliderWidth}px ${valueSliderHeight}px ${valueSliderRadius}px ${valueSliderPropagation}px rgb(${r}, ${g}, ${b}, ${valueSliderOpacity})`;
+           //Se asigna a colorRgba la estructura de los colores que se van modifiando para el color de la caja.
            const colorRgba = ` rgb(${r}, ${g}, ${b}, ${valueSliderOpacity})`;
            
+           //Se asignan las constantes al style de la caja (color y sombra). 
             caja.style.color = colorRgba;
             caja.style.boxShadow = value;
 
+            //Se colocan los valores actuales del Input tipo Color al Input tipo text para que se vean reflejados en texto.
            document.getElementById('shadowColorInputText').value = e.target.value;
         });
 
         //InputText ShadowColor
+            //Elemento Input tipo text para definir el color del la sombra.
+            
         const shadowColorInputText= document.getElementById('shadowColorInputText');
         shadowColorInputText.addEventListener('input', e => {
 
@@ -114,36 +124,43 @@ const ShadowScreen = () => {
            const valueSliderRadius = sliderRadius.value;
            //Valor del SliderPropagation (Barra).
            const valueSliderPropagation = sliderPropagation.value;
-          
-            const inputValue = e.target.value;
 
-            const color = inputValue;
+            //Asignacion del valor target a const color donde color es una string como -> ("#a5a8b1")
+            const color = e.target.value;
+            
+            //El valor 1 y 2 del string corresponden al valor exadecimal del color red, 3 y 4 corresponde a color green, 5 y 6 corresponden a color blue
             const red = color[1]+color[2];
             const green = color[3]+color[4];
             const  blue= color[5]+color[6];
-
+            
+            //Los string se convierten en numeros base 16
             const r = parseInt(red,16);
             const g = parseInt(green,16);
             const b = parseInt(blue,16);
 
-            valueRed1.innerHTML = r;
-            valueRed2.innerHTML = r;
-            valueRed3.innerHTML = r;
+            //para cada elemto con la clase valueRedClass se le asigna el valor de r
+            valueRedClass.forEach(function(elemento, indice, array) {                                        
+                elemento.innerHTML = r;                                     
+            });
+            //para cada elemto con la clase valueGreenClass se le asigna el valor de g
+            valueGreenClass.forEach(function(elemento, indice, array) {                                        
+                elemento.innerHTML = g;                                     
+            });
+            //para cada elemto con la clase valueBluenClass se le asigna el valor de b
+            valueBlueClass.forEach(function(elemento, indice, array) {                                        
+                elemento.innerHTML = b;                                     
+            });
 
-            valueGreen1.innerHTML = g;
-            valueGreen2.innerHTML = g;
-            valueGreen3.innerHTML = g;
-
-            valueBlue1.innerHTML = b;
-            valueBlue2.innerHTML = b;
-            valueBlue3.innerHTML = b;
-
+            //Se asigna a value la estructura con los valores que se van modifiando para la sombra de la caja.
            const value = ` ${valueSliderWidth}px ${valueSliderHeight}px ${valueSliderRadius}px ${valueSliderPropagation}px rgb(${r}, ${g}, ${b}, ${valueSliderOpacity})`;
-           const colorRgba = ` rgb(${r}, ${g}, ${b}, ${valueSliderOpacity})`;
-           
+            //Se asigna a colorRgba la estructura de los colores que se van modifiando para el color de la caja.
+            const colorRgba = ` rgb(${r}, ${g}, ${b}, ${valueSliderOpacity})`;
+
+            //Se asignan las constantes al style de la caja (color y sombra). 
             caja.style.color = colorRgba;
             caja.style.boxShadow = value;
 
+            //Se colocan los valores actuales del Input tipo text al Input tipo color para que se vea reflejado el color en el input.
             document.getElementById('shadowcolorInputColor').value = e.target.value;  
         });
 
@@ -230,9 +247,9 @@ const ShadowScreen = () => {
                         <div className="box box-r">
                             <div className="caja" id="caja">
                                 <div className="caja-text">
-                                    <p>-webkit-box-shadow: <span className="widthClass" id="valueWidth1">60</span>px <span className="heightClass" id="valueHeight1">60</span>px <span className="shadowRadiusClass" id="valueRadius1">60</span>px <span className="progationclass" id="valuePropagation1">60</span>px rgba(<span id="valueRed1">42</span>, <span id="valueGreen1">79</span>, <span id="valueBlue1">180</span>, <span className="valueOpacityClass" id="valueOpacity1">0.6</span>);</p>
-                                       <p>-moz-box-shadow: <span className="widthClass" id="valueWidth2">60</span>px <span className="heightClass" id="valueHeight2">60</span>px <span className="shadowRadiusClass" id="valueRadius2">60</span>px <span className="progationclass" id="valuePropagation2">60</span>px rgba(<span id="valueRed2">42</span>, <span id="valueGreen2">79</span>, <span id="valueBlue2">180</span>, <span className="valueOpacityClass" id="valueOpacity2">0.6</span>);</p>
-                                            <p>box-shadow: <span className="widthClass" id="valueWidth3">60</span>px <span className="heightClass" id="valueHeight3">60</span>px <span className="shadowRadiusClass" id="valueRadius3">60</span>px <span className="progationclass" id="valuePropagation3">60</span>px rgba(<span id="valueRed3">42</span>, <span id="valueGreen3">79</span>, <span id="valueBlue3">180</span>, <span className="valueOpacityClass" id="valueOpacity3">0.6</span>);</p>
+                                    <p>-webkit-box-shadow: <span className="widthClass" id="valueWidth1">60</span>px <span className="heightClass" id="valueHeight1">60</span>px <span className="shadowRadiusClass" id="valueRadius1">60</span>px <span className="progationclass" id="valuePropagation1">60</span>px rgba(<span className="valueRedClass" id="valueRed1">42</span>, <span className="valueRedClass" id="valueGreen1">79</span>, <span className="valueBlueClass" id="valueBlue1">180</span>, <span className="valueOpacityClass" id="valueOpacity1">0.6</span>);</p>
+                                       <p>-moz-box-shadow: <span className="widthClass" id="valueWidth2">60</span>px <span className="heightClass" id="valueHeight2">60</span>px <span className="shadowRadiusClass" id="valueRadius2">60</span>px <span className="progationclass" id="valuePropagation2">60</span>px rgba(<span className="valueRedClass" id="valueRed2">42</span>, <span className="valueRedClass" id="valueGreen2">79</span>, <span className="valueBlueClass" id="valueBlue2">180</span>, <span className="valueOpacityClass" id="valueOpacity2">0.6</span>);</p>
+                                            <p>box-shadow: <span className="widthClass" id="valueWidth3">60</span>px <span className="heightClass" id="valueHeight3">60</span>px <span className="shadowRadiusClass" id="valueRadius3">60</span>px <span className="progationclass" id="valuePropagation3">60</span>px rgba(<span className="valueRedClass" id="valueRed3">42</span>, <span className="valueRedClass" id="valueGreen3">79</span>, <span className="valueBlueClass" id="valueBlue3">180</span>, <span className="valueOpacityClass" id="valueOpacity3">0.6</span>);</p>
                                 </div>
                             </div>
                         </div>
--- src/components/rangeBar/RangeBar.js
+++ src/components/rangeBar/RangeBar.js
@@ -1,74 +1,87 @@
 import React, { useEffect } from 'react'
 
+//Componente RangeBar con  los atributos recibidos por props
 const RangeBar = ({id,name,valueMin,valueMax,valueDefault,valueDown,nameClass}) => {
 
+    //se utiliza un useEffect para crear los Id de los elementos html despues de la creacion del documento html
+    //Para más información checar el ciclo de vida de un componente en react
     useEffect(() => {
-        
+
+        //Creacion de referencia a Elementos HTML.
         const barra = document.getElementById(id);
         const downValue  = document.getElementById(valueDown);
         const className = document.querySelectorAll(nameClass);
         
         barra.addEventListener('input',function(){
+            //Porcentaje del defaultValue entre valor Minimo y ValorMaximo. 
+            //Para funcionamiento del desplazamiento del color de la rangeBar
             const porcentajePropagation = ((barra.value - barra.min) / (barra.max - barra.min) * 100 ); 
             const colorBarraPropagation = `linear-gradient(90deg, rgb(117, 241, 252) ${porcentajePropagation}%,rgb(214, 214, 214)${porcentajePropagation}%)`;
             barra.style.background = colorBarraPropagation;
         
+            //Creacion de referencia a Elementos HTML.
             const caja = document.getElementById('caja');
-        
-                        const sliderOpacity = document.getElementById('myRangeOpacity').value;
-                        const sliderWidth = document.getElementById('myRangeWidth').value;
-                        const sliderHeight = document.getElementById('myRangeHeight').value;
-                        const sliderRadius = document.getElementById('myRangeRadius').value;
-                        const sliderPropagation = document.getElementById('myRangePropagation').value;
-                        const shadowcolorInputColor= document.getElementById('shadowcolorInputColor');
+            const sliderOpacity = document.getElementById('myRangeOpacity').value;
+            const sliderWidth = document.getElementById('myRangeWidth').value;
+            const sliderHeight = document.getElementById('myRangeHeight').value;
+            const sliderRadius = document.getElementById('myRangeRadius').value;
+            const sliderPropagation = document.getElementById('myRangePropagation').value;
+            const shadowcolorInputColor= document.getElementById('shadowcolorInputColor');
 
-                        //Valor del Slider (Barra).
-                       const valueSliderOpacity = sliderOpacity*0.01;
-                       //Valor del SliderWidth (Barra).
-                       const valueSliderWidth = sliderWidth;
-                       //Valor del SliderHeight (Barra).
-                       const valueSliderHeight = sliderHeight;
-                       //Valor del SliderRadius (Barra).
-                       const valueSliderRadius = sliderRadius;
-                       //Valor del SliderPropagation (Barra).
-                       const valueSliderPropagation = sliderPropagation;
+            //Valor del Slider (Barra).
+            const valueSliderOpacity = sliderOpacity*0.01;
+            //Valor del SliderWidth (Barra).
+            const valueSliderWidth = sliderWidth;
+            //Valor del SliderHeight (Barra).
+            const valueSliderHeight = sliderHeight;
+            //Valor del SliderRadius (Barra).
+            const valueSliderRadius = sliderRadius;
+            //Valor del SliderPropagation (Barra).
+            const valueSliderPropagation = sliderPropagation;
 
-                       if (id === 'myRangeOpacity') {
-                           downValue.innerHTML =  valueSliderOpacity.toFixed(2);
-                            barra.oninput = function(){
-                                //Asignacion del valor del slider a la caja de texto
-                                downValue.innerHTML = valueSliderOpacity.toFixed(2);
-                                className.forEach(function(elemento, indice, array) {                                        
-                                    elemento.innerHTML = valueSliderOpacity.toFixed(2);                                     
-                                });
-                            }
-                       } else {
-                           downValue.innerHTML =  barra.value;
-                            barra.oninput = function(){
-                                const thisValue = parseInt(this.value);
-                                //Asignacion del valor del slider a la caja de texto
-                                downValue.innerHTML = thisValue;
-                                className.forEach(function(elemento, indice, array) {
-                                        elemento.innerHTML = thisValue; 
-                                });
-                            }
-                       }            
+            //Condicion exclusiva para 'myRangeOpacity' por que su valor son decimales (entre 0 y 1) y el rangeBar sus valores son en unidades (del 1 al 100 ó 200 ó etc...)
+            if (id === 'myRangeOpacity') {
+                //Se asigna el valor mostrado en la parte inferior derecha del componente limitandolo a 2 numeros
+                downValue.innerHTML =  valueSliderOpacity.toFixed(2);
 
-                        const inputValue = shadowcolorInputColor.value;
+                barra.oninput = function(){
+                    //Asignacion del valor del slider a los elementos con la className indicada y a la caja de texto mostrado en la parte inferior derecha del componente limitandolo a 2 numeros
+                    downValue.innerHTML = valueSliderOpacity.toFixed(2);
+                    className.forEach(function(elemento, indice, array) {                                        
+                        elemento.innerHTML = valueSliderOpacity.toFixed(2);                                     
+                    });
+                }
+            } else {
+                downValue.innerHTML =  barra.value;
+                barra.oninput = function(){
+                    const thisValue = parseInt(this.value);
+                    //Asignacion del valor del slider a la caja de texto
+                    downValue.innerHTML = thisValue;
+                    className.forEach(function(elemento, indice, array) {
+                            elemento.innerHTML = thisValue; 
+                    });
+                }
+            }            
 
-                        const color = inputValue;
-                        const red = color[1]+color[2];
-                        const green = color[3]+color[4];
-                        const  blue= color[5]+color[6];
+            //Se asigna el valor del color de la sombra Inputcolor donde color es una string como -> ("#a5a8b1").
+            const color =  shadowcolorInputColor.value;
+            //El valor 1 y 2 del string corresponden al valor exadecimal del color red, 3 y 4 al color green y 5 y 6 al color blue.
+            const red = color[1]+color[2];
+            const green = color[3]+color[4];
+            const  blue= color[5]+color[6];
 
-                        const r = parseInt(red,16);
-                        const g = parseInt(green,16);
-                        const b = parseInt(blue,16);        
-        
-                    const value = ` ${valueSliderWidth}px ${valueSliderHeight}px ${valueSliderRadius}px ${valueSliderPropagation}px rgb(${r}, ${g}, ${b}, ${valueSliderOpacity})`;
-                    const colorRgba = ` rgb(${r}, ${g}, ${b}, ${valueSliderOpacity})`;         
-                    caja.style.color = colorRgba;
-                    caja.style.boxShadow = value;
+            //Los string se convierten en numeros base 16
+            const r = parseInt(red,16);
+            const g = parseInt(green,16);
+            const b = parseInt(blue,16);        
+
+        //Se asigna a value la estructura con los valores que se van modifiando para la sombra de la caja.
+        const value = ` ${valueSliderWidth}px ${valueSliderHeight}px ${valueSliderRadius}px ${valueSliderPropagation}px rgb(${r}, ${g}, ${b}, ${valueSliderOpacity})`;
+        //Se asigna a colorRgba la estructura de los colores que se van modifiando para el color de la caja.
+        const colorRgba = ` rgb(${r}, ${g}, ${b}, ${valueSliderOpacity})`;         
+        //Se asignan las constantes al style de la caja (color y sombra). 
+        caja.style.color = colorRgba;
+        caja.style.boxShadow = value;
         });  
     } );
 
